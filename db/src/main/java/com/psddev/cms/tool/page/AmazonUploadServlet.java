43d289bb570af022f72687c3c691d8a66ce9077f
Ryan-Anderson <ryan@perfectsensedigital.com>
2015-07-22 10:00:02 -0400

    Adds the ability to generate storage item path prior to upload

--- db/src/main/java/com/psddev/cms/tool/page/AmazonUploadServlet.java
+++ db/src/main/java/com/psddev/cms/tool/page/AmazonUploadServlet.java
@@ -3,24 +3,55 @@ package com.psddev.cms.tool.page;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.codec.binary.Base64;
+import com.google.common.collect.ImmutableMap;
+import com.psddev.cms.db.ToolUi;
+import com.psddev.cms.tool.PageServlet;
+import com.psddev.cms.tool.ToolPageContext;
+import com.psddev.dari.db.ObjectField;
+import com.psddev.dari.db.ObjectType;
 import com.psddev.dari.util.AmazonStorageItem;
+import com.psddev.dari.util.ObjectUtils;
 import com.psddev.dari.util.RoutingFilter;
 import com.psddev.dari.util.Settings;
 import com.psddev.dari.util.StorageItem;
 import com.psddev.dari.util.StringUtils;
+import com.psddev.dari.util.WebPageContext;
 
-@RoutingFilter.Path(application = "cms", value = "amazonAuth")
-class AmazonUploadServlet extends HttpServlet {
+@RoutingFilter.Path(application = "cms", value = "amazonUploader")
+class AmazonUploadServlet extends PageServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected String getPermissionId() {
+        return null;
+    }
 
-        String storageSetting = !StringUtils.isBlank(request.getParameter("storageSetting")) ? request.getParameter("storageSetting") : StorageItem.DEFAULT_STORAGE_SETTING;
+    @Override
+    protected void doService(ToolPageContext page) throws IOException, ServletException {
+
+        if (page.requireUser()) {
+            return;
+        }
+
+        String action = page.param(String.class, "action");
+
+        if (StringUtils.isBlank(action)) {
+            throw new ServletException("Missing [action] parameter");
+        }
+
+        if (action.equals("sign")) {
+            signRequest(page);
+        } else if (action.equals("getPath")) {
+            getStoragePath(page);
+        }
+    }
+
+    private void signRequest(WebPageContext page) throws ServletException, IOException {
+        String storageSetting = !StringUtils.isBlank(page.param(String.class, "storageSetting"))
+                ? page.param(String.class, "storageSetting")
+                : StorageItem.DEFAULT_STORAGE_SETTING;
 
         String secret = Settings.get(String.class, StorageItem.SETTING_PREFIX + "/" + storageSetting + "/" + AmazonStorageItem.SECRET_SETTING);
 
@@ -28,10 +59,42 @@ class AmazonUploadServlet extends HttpServlet {
             throw new ServletException(StorageItem.SETTING_PREFIX + storageSetting + "/secret not found in your context.xml");
         }
 
-        byte[] rawHmac = StringUtils.hmacSha1(secret, request.getParameter("to_sign"));
+        byte[] rawHmac = StringUtils.hmacSha1(secret, page.param(String.class, "to_sign"));
         String result = new String(Base64.encodeBase64(rawHmac));
 
+        HttpServletResponse response = page.getResponse();
         response.setContentType("text/html");
         response.getWriter().write(result);
+    }
+
+    private void getStoragePath(WebPageContext page) throws ServletException, IOException {
+        String typeId = page.param(String.class, "typeId");
+        String fieldName = page.param(String.class, "fieldName");
+        String fileName = page.param(String.class, "fileName");
+
+        if (StringUtils.isBlank(typeId) || StringUtils.isBlank(fieldName) || StringUtils.isBlank(fileName)) {
+            throw new ServletException("Missing parameters:"
+                    + (StringUtils.isBlank(typeId) ? " [typeId]" : "")
+                    + (StringUtils.isBlank(fieldName) ? " [fieldName]" : "")
+                    + (StringUtils.isBlank(fileName) ? " [fileName]" : ""));
+        }
+
+        ObjectType type = ObjectType.getInstance(typeId);
+
+        if (type == null) {
+            throw new ServletException("No ObjectType exists with [id]: " + typeId);
+        }
+
+        ObjectField field = type.getField(fieldName);
+
+        if (field == null) {
+            throw new ServletException("No ObjectField exists for type [" + typeId + "] with name  [" + fieldName + "]");
+        }
+
+        HttpServletResponse response = page.getResponse();
+        response.setContentType("application/json");
+        page.write(ObjectUtils.toJson(
+                ImmutableMap.of(
+                        "filePath", field.as(ToolUi.class).getStoragePath(null, fileName))));
     }
 }
